74af4075332e560c0b02ed5c52a2abf808bf52f4
Ken Snyder <kendsnyder@gmail.com>
2019-09-30 11:53:25 -0600

    [master] update build process to produce ie11-friendly js

---
+++ entry.js
@@ -0,0 +1,7 @@
+import { barHorizontal } from './src/charts/barHorizontal.js';
+import { barVertical } from './src/charts/barVertical.js';
+import { bubbles } from './src/charts/bubbles.js';
+import { stream } from './src/charts/stream.js';
+import { clearChart } from './src/charts/helpers/clearChart.js';
+
+export { barHorizontal, barVertical, bubbles, stream, clearChart };
